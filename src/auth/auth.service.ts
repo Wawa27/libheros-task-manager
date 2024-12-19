15a76f267388c4e9072125d6cfcd5ad7cbe29d58
Wawa27 <32011804+Wawa27@users.noreply.github.com>
2024-12-19 12:15:09 +0100

    feat: add auth routes (#4)

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../users/user.service'; // Assuming you already have a User service
+import { User } from '../users/user.entity';
+import * as bcrypt from 'bcrypt';
+import { RegisterDto } from './dto/register.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(registerDto: RegisterDto): Promise<User> {
+    const { password } = registerDto;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    return await this.userService.create({
+      ...registerDto,
+      password: hashedPassword,
+    });
+  }
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userService.findOneByEmail(email);
+    if (!user) {
+      return null;
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return null;
+    }
+
+    return user;
+  }
+
+  async login(user: User) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
